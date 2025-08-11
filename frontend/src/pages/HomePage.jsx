e36225ae4e823cc72c0447b4388c801c7e671b88
Pratibha Sharma <pratibhaa625@gmail.com>
2025-08-11 21:35:25 +0545

    cors and homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import { use, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import RatelimitedUI from "../components/RatelimitedUI";
 import NoteCard from "../components/NoteCard";
@@ -11,50 +11,41 @@ const HomePage = () => {
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    const fetchNotes= async () => {
-      try{
-        // const res= await fetch("http://localhost:5001/api/notes");
-        // const data= await res.json();
-        const res=await axios.get("http://localhost:5001/api/notes");
-        console.log(res.data);
+    const fetchNotes = async () => {
+      setLoading(true);
+      try {
+        const res = await axios.get("http://localhost:5001/api/notes");
         setNotes(res.data);
         setIsRatelimited(false);
-
-      }catch(error){
-        console.log("Error fetching notes");
-        console.log(error);
-        if(error.response?.status==429){
+      } catch (error) {
+        if (error.response?.status === 429) {
           setIsRatelimited(true);
-        }else{
+        } else {
           toast.error("Error fetching notes");
         }
-      }
-      finally {
+      } finally {
         setLoading(false);
-      }      
-    }
-  },[])
+      }
+    };
+    fetchNotes();
+  }, []);
 
   return (
     <div className='min-h-screen'>
       <Navbar />
-      { isRatelimited && <RatelimitedUI />} 
+      {isRatelimited && <RatelimitedUI />}
       <div className="max-w-7xl mx-auto p-4 mt-6">
-        {loading && <div className="text-center text-primary py-19 ">Loading...</div>}
-
-        {notes.length > 0 && !isRatelimited (
+        {loading && <div className="text-center text-primary py-19">Loading...</div>}
+        {notes.length > 0 && !isRatelimited && (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            {notes.map((note)=>(
-              <NoteCard key={note.id} title={note.title} content={note.content} />
+            {notes.map((note) => (
+              <NoteCard key={note._id || note.id} title={note.title} content={note.content} />
             ))}
           </div>
-          
         )}
-
-
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
