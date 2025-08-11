09ab9fc9caac43cd54a051f71071ba7e9e13d39e
Pratibha Sharma <pratibhaa625@gmail.com>
2025-08-11 15:58:37 +0545

    Navbar

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Route, Routes } from "react-router";
+import CreatePage from "./pages/CreatePage.jsx";
+import NoteDetailPage from "./pages/NoteDetailPage.jsx";
+import HomePage from "./pages/HomePage.jsx";
+
+const App = () => {
+  return (
+    <div data-theme="forest">      
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/note/:id" element={<NoteDetailPage />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
