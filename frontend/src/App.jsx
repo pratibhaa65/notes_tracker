8d6998767f2bbb43d82086c868a54699102a529d
Pratibha Sharma <pratibhaa625@gmail.com>
2025-08-12 18:54:08 +0545

    NoteDetailspage updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,13 @@
 import { Route, Routes } from "react-router";
-import CreatePage from "./pages/CreatePage.jsx";
-import NoteDetailPage from "./pages/NoteDetailPage.jsx";
-import HomePage from "./pages/HomePage.jsx";
+
+import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
+import NoteDetailPage from "./pages/NoteDetailPage";
 
 const App = () => {
   return (
     <div className="relative h-full w-full">
-      <div
-        className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_60%,#0ff0f040_100%)]"
-      />
+      <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_60%,#00FF9D40_100%)]" />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
@@ -17,5 +16,4 @@ const App = () => {
     </div>
   );
 };
-
 export default App;
