9a4559927a87e03a22e9a5166817a06456dd2c9f
Pratibha Sharma <pratibhaa625@gmail.com>
2025-08-12 07:56:53 +0545

    createpage updated

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,9 +1,94 @@
-import React from 'react'
+import { useState } from "react";
+import { Link } from "react-router";
+import { ArrowLeftIcon } from "lucide-react";
+import api from "../lib/axios";
+import { toast } from "react-hot-toast";
+import { useNavigate } from "react-router";
 
 const CreatePage = () => {
-  return (
-    <div>CreatePage</div>
-  )
-}
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
-export default CreatePage
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!title.trim() || !content.trim()) {
+      alert("Please fill in all fields");
+      return;
+    }
+    setLoading(true);
+    try {
+      await api.post("/notes", {
+        title,
+        content,
+      });
+      toast.success("Note created successfully!");
+      navigate("/");
+    } catch (error) {
+      console.error("Error creating note:", error);
+      if (error.response.status === 429) {
+        toast.error("Slow down! You're creating notes too quickly.", {
+          duration: 3000,
+          position: "top-center",
+        });
+      } else {
+        toast.error("Error creating note");
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="min-h-screen bg-base-200">
+      <div className="container mx-auto px-4 py-8">
+        <div className="max-w-2xl  mx-auto">
+          <Link to={"/"} className="btn btn-ghost mb-6">
+            <ArrowLeftIcon />
+            Back to Notes
+          </Link>
+          <div className="card bg-base-100 shadow-xl">
+            <div className="card-body">
+              <h2 className="card-title text-2xl mb-4">Create New Note</h2>
+              <form onSubmit={handleSubmit}>
+                <div className="form-control mb-4">
+                  <label className="label">
+                    <span className="label-text">Title</span>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="Note Title"
+                    className="input input-bordered"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                    required
+                  />
+                </div>
+                <div className="form-control mb-4">
+                  <label className="label">
+                    <span className="label-text">Content</span>
+                  </label>
+                  <textarea
+                    className="textarea textarea-bordered"
+                    placeholder="Write your note here..."
+                    value={content}
+                    onChange={(e) => setContent(e.target.value)}
+                    required
+                  />
+                </div>
+                <div className="card-actions justify-end">
+                  <button type="submit" className="btn btn-primary">
+                    {loading ? "Creating..." : "Create Note"}
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CreatePage;
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import RateLimitedUI from "../components/RatelimitedUI.jsx";
 import NoteCard from "../components/NoteCard";
-import axios from "axios";
+import api from "../lib/axios";
 import { toast } from "react-hot-toast";
 
 const HomePage = () => {
@@ -14,7 +14,7 @@ const HomePage = () => {
     const fetchNotes = async () => {
       setLoading(true);
       try {
-        const res = await axios.get("http://localhost:5001/api/notes");
+        const res = await api.get("/notes");
         setNotes(res.data);
         setIsRatelimited(false);
       } catch (error) {
@@ -31,15 +31,21 @@ const HomePage = () => {
   }, []);
 
   return (
-    <div className='min-h-screen'>
+    <div className="min-h-screen">
       <Navbar />
       {isRatelimited && <RateLimitedUI />} {/* Use correct component name */}
       <div className="max-w-7xl mx-auto p-4 mt-6">
-        {loading && <div className="text-center text-primary py-19">Loading...</div>}
+        {loading && (
+          <div className="text-center text-primary py-19">Loading...</div>
+        )}
         {notes.length > 0 && !isRatelimited && (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {notes.map((note) => (
-              <NoteCard key={note._id || note.id} title={note.title} content={note.content} />
+              <NoteCard
+                key={note._id || note.id}
+                title={note.title}
+                content={note.content}
+              />
             ))}
           </div>
         )}
